5710a109b16682deb2590b80afe39f6442ec1799
grumpycat711 <eugeni.puigdomenech@gmail.com>
2022-10-29 13:58:34 +0200

    added-lost-files

---
+++ src/Attacker.java
@@ -0,0 +1,30 @@
+public interface Attacker {
+
+    void attack();
+
+    //if (characterType == warrior) {
+    //
+    //        if (stamina >= 5) {
+    //          System.log.out("Heavy attack!")
+    //        HPDefender = HPDefender - strengthAttacker
+    //          staminaAttacker = staminaAttacker - 5;
+    //       } else{
+    //      System.log.out("Weak attack!")
+    //     HPDefender = HPDefender - (strengthAttacker/2)
+    //      staminaAttacker = staminaAttacker + 1;
+    // }
+    //}
+    //else{
+    //
+    // //        if (mana >= 5) {
+    //          System.log.out("Fireball casted")
+    //           HPDefender = HPDefender - IntelligenceAttacker
+    //              manaAttacker = manaAttacker - 5;
+    //           } else{
+    //             System.log.out("Staff hit")
+    //         HPDefender = HPDefender - 2;
+    //          manaAttacker = manaAttacker + 1;
+    //     }
+    //
+    //
+}
