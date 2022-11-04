af1b405f267f7ecbfa3901775a07433bff17d637
ainga-ri <alfred.inga.rios@gmail.com>
2022-11-05 00:31:16 +0100

    assigned charstats and iteration tool for strings

--- src/main/java/Lobby.java
+++ src/main/java/Lobby.java
@@ -7,7 +7,7 @@ public class Lobby {
 
     Lobby() {}
 
-    public void teamCapacity(int creationMode, Scanner input) {
+    public void teamCapacity(int creationMode, Scanner input) throws InterruptedException {
         int size;
         System.out.println("How many characters would you like on your team?");
         size = input.nextInt();
@@ -20,28 +20,78 @@ public class Lobby {
                 creatingFomCSV();
         }
     }
-    public void creatingCostumized(int size, Scanner input) {
+    private void showCharacters() throws InterruptedException {
+        // Show it like is charging, like old...
+        int types = CharacterType.values().length;
+        String character;
+        int i = 1;
+        for (CharacterType charType: CharacterType.values()) {
+            System.out.printf("[%d] - ", i);
+            character = "%s\n".formatted(charType);
+            makeItSlow(character);
+            i++;
+        }
+    }
+    private void makeItSlow(String arcade) throws InterruptedException {
+        for (int i = 0; i < arcade.length(); i++) {
+            Thread.sleep(220);
+            System.out.print(arcade.charAt(i));
+        }
+    }
+    public void creatingCostumized(int size, Scanner input) throws InterruptedException {
         String type, name, hp, energy, power;
-
         String[] charStats = new String[5];
         boolean comparision;
         for (int i = 0; i < size; i++) {
-            do {
-                String creationChar = """
-                        What do you want to create?
-                        ================
-                        [1] - WIZARD
-                        [2] - WARRIOR
-                        """.formatted();
-                type = input.nextLine();
-                comparision = Integer.parseInt(type) < 0 || Integer.parseInt(type) > 2;
-                if (comparision)
-                    System.out.println("You entered a invalid number. Try again");
-            } while (comparision);
+            charStats[0] = assignTypeOfCharacter(input);
             System.out.println("Enter the name:");
-            teamOne.add(charStats);
+            charStats[1] = input.nextLine();
+            charStats[2] = AssignValueAndShowDialogueWithIntegers("Enter the health", input);
+            charStats[3] = AssignValueAndShowDialogueWithIntegers("Enter the energy", input);
+            charStats[4] = AssignValueAndShowDialogueWithIntegers("Enter the power", input);
+            
         }
+    }
 
+    private String assignTypeOfCharacter(Scanner input) throws InterruptedException {
+        String type;
+        boolean comparision;
+        do {
+            System.out.println("""
+                    What do you want to create?
+                    ================
+                    """);
+            showCharacters();
+            type = input.nextLine();
+            comparision = Integer.parseInt(type) < 0 || Integer.parseInt(type) > 2;
+            if (comparision)
+                System.out.println("You entered an invalid number. Try again");
+        } while (comparision);
+        return type;
+    }
+
+    private String AssignValueAndShowDialogueWithIntegers(String dialogue, Scanner input) {
+        boolean comparision;
+        String value;
+        do {
+            System.out.println(dialogue);
+            value = input.nextLine();
+            comparision = isNumber(value);
+            if (!comparision)
+                System.out.println("You have entered an invalid number. Try again");
+        } while (!comparision);
+        return value;
+    }
+
+    private boolean isNumber(String value) {
+        boolean isNum = true;
+        int index = 0;
+        while (index < value.length() && isNum) {
+            if (!(java.lang.Character.isDigit(value.charAt(index))))
+                isNum = false;
+            index++;
+        }
+        return isNum;
     }
     public void creatingRandom() {
         //TODO
--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 public class Menu {
     Lobby lobby;
 
-    public void main() {
+    public void main() throws InterruptedException {
         Scanner input = new Scanner(System.in);
+        String title = """
+                  ________            __  __          __     _____                __   ______               ______                  \s
+                 /_  __/ /_  ___     / / / /___  ____/ /__  / __(_)___  ___  ____/ /  / ____/_  ____  __   / ____/___ _____ ___  ___\s
+                  / / / __ \\/ _ \\   / / / / __ \\/ __  / _ \\/ /_/ / __ \\/ _ \\/ __  /  / / __/ / / / / / /  / / __/ __ `/ __ `__ \\/ _ \\
+                 / / / / / /  __/  / /_/ / / / / /_/ /  __/ __/ / / / /  __/ /_/ /  / /_/ / /_/ / /_/ /  / /_/ / /_/ / / / / / /  __/
+                /_/ /_/ /_/\\___/   \\____/_/ /_/\\__,_/\\___/_/ /_/_/ /_/\\___/\\__,_/   \\____/\\__,_/\\__, /   \\____/\\__,_/_/ /_/ /_/\\___/\s
+                                                                                               /____/                               \s
+                """;
         String menuMessage = """
-                The Undefined Guy Game
                 ----------------
                 How would you like to play?
                 [1] - Player vs IA
@@ -24,7 +31,8 @@ public class Menu {
                 [2] - Full random
                 [3] - Import from CSV
                 """;
-
+        Lobby clara = new Lobby();
+        clara.creatingCostumized(2, input);
         
     }
 
