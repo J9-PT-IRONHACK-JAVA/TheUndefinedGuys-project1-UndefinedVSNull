ff5948d4d0ed86556a354b228002af68f7ceb060
ainga-ri <alfred.inga.rios@gmail.com>
2022-11-04 17:34:50 +0100

    working on lobbyfunctions

--- src/main/java/Lobby.java
+++ src/main/java/Lobby.java
@@ -1,17 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lobby {
-    //private Team teamOne;
-    //private Team teamTwo;
+    private Team teamOne;
+    private Team teamTwo;
 
     Lobby() {}
 
-    public void creatingCostumized() {
+    public void teamCapacity(int creationMode, Scanner input) {
+        int size;
+        System.out.println("How many characters would you like on your team?");
+        size = input.nextInt();
+        switch (creationMode) {
+            case 1:
+                creatingCostumized(size, input);
+            case 2:
+                creatingRandom();
+            case 3:
+                creatingFomCSV();
+        }
+    }
+    public void creatingCostumized(int size, Scanner input) {
+        String type, name, hp, energy, power;
+
+        String[] charStats = new String[5];
+        boolean comparision;
+        for (int i = 0; i < size; i++) {
+            do {
+                String creationChar = """
+                        What do you want to create?
+                        ================
+                        [1] - WIZARD
+                        [2] - WARRIOR
+                        """.formatted();
+                type = input.nextLine();
+                comparision = Integer.parseInt(type) < 0 || Integer.parseInt(type) > 2;
+                if (comparision)
+                    System.out.println("You entered a invalid number. Try again");
+            } while (comparision);
+            System.out.println("Enter the name:");
+            teamOne.add(charStats);
+        }
 
     }
     public void creatingRandom() {
-
+        //TODO
     }
     public void creatingFomCSV() {
-
+        //TODO
     }
     @Override
     public String toString() {
--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Menu {
     Lobby lobby;
 
     public void main() {
+        Scanner input = new Scanner(System.in);
         String menuMessage = """
                 The Undefined Guy Game
                 ----------------
@@ -10,7 +13,19 @@ public class Menu {
                 [2] - Player vs Player
                 [3] - IA vs IA
                 """;
-        // Segun lo que ponga
+                //TODO
+                //--------------formatted-------------();
+        // Segun lo que ponga --> 1
+
+        String teamCreationMode = """
+                ----------------
+                How would you like to create your team?
+                [1] - Full customized
+                [2] - Full random
+                [3] - Import from CSV
+                """;
+
+        
     }
 
 }
